2fd6f5fff70c50abfcb9f891b73444bada0faec3
Mohammad Al Zaroo <211008@ppu.edu.ps>
2026-04-13 21:48:21 +0300

    enhance UI in send_text page

--- lib/features/send_text/send_text_page.dart
+++ lib/features/send_text/send_text_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mqtt_test/features/shared_widgets/mqtt_logo.dart';
 import '../../core/mqtt/mqtt_service.dart';
 
 class SendTextPage extends StatefulWidget {
@@ -20,14 +21,23 @@ class _SendTextPageState extends State<SendTextPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Send Text")),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(controller: controller),
-            ElevatedButton(onPressed: send, child: const Text("Send")),
-          ],
-        ),
+      body: Stack(
+        children: [
+          MqttLogo(),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                    controller: controller,
+                    decoration:
+                        const InputDecoration(labelText: "Enter text to send")),
+                ElevatedButton(onPressed: send, child: const Text("Send")),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
