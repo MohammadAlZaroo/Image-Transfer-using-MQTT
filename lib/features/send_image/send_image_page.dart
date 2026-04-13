8ebe001aaf8538629be117e2a584a727f688af3f
Mohammad Al Zaroo <211008@ppu.edu.ps>
2026-04-13 22:22:50 +0300

    remove uneseccery methods from send_image page

--- lib/features/send_image/send_image_page.dart
+++ lib/features/send_image/send_image_page.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mqtt_test/features/shared_widgets/mqtt_logo.dart';
-import '../../core/image_processing/image_converter.dart';
-import '../../core/mqtt/mqtt_service.dart';
 
 class SendImagePage extends StatelessWidget {
   const SendImagePage({super.key});
-
-  void sendImage(String name) async {
-    final bytes = await ImageConverter.convertAssetToMono(name);
-    MqttService().publishImage(bytes);
-  }
 
   @override
   Widget build(BuildContext context) {
